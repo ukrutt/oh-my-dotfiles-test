57db6811588bd1ce4d671067cb381928fd1eedf9
Henrik Holm <henrik@henrikholm.com>
2014-04-08 08:08:42 -0500

    First shot at the downloader

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,47 +1,22 @@
+#/usr/bin/env sh
+
 set -e
 
-if [ ! -n "$ZSH" ]; then
-  ZSH=~/.oh-my-zsh
+# Check that we have some essential programs already installed.
+hash zsh >/dev/null 2>&1 || { echo >&2 "Need zsh.  Aborting."; exit 1; }
+hash git >/dev/null 2>&1 || { echo >&2 "Need git.  Aborting."; exit 1; }
+
+# Check that the 'dotfiles' aren't already installed
+if [ ! -n "$DOTFILES" ]; then
+  DOTFILES=~/.oh-my-dotfiles
 fi
 
-if [ -d "$ZSH" ]; then
-  echo "\033[0;33mYou already have Oh My Zsh installed.\033[0m You'll need to remove $ZSH if you want to install"
+if [ -d "$DOTFILES" ]; then
+  echo "\033[0;33mYou already have Oh My Dotfiles installed.\033[0m You'll need to remove $DOTFILES if you want to install"
   exit
 fi
 
-echo "\033[0;34mCloning Oh My Zsh...\033[0m"
-hash git >/dev/null && /usr/bin/env git clone https://github.com/robbyrussell/oh-my-zsh.git $ZSH || {
-  echo "git not installed"
-  exit
-}
+echo "\033[0;34mCloning Oh My Dotfiles...\033[0m"
+/usr/bin/env git clone https://github.com/ukrutt/oh-my-dotfiles-test.git $DOTFILES
 
-echo "\033[0;34mLooking for an existing zsh config...\033[0m"
-if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]; then
-  echo "\033[0;33mFound ~/.zshrc.\033[0m \033[0;32mBacking up to ~/.zshrc.pre-oh-my-zsh\033[0m";
-  mv ~/.zshrc ~/.zshrc.pre-oh-my-zsh;
-fi
-
-echo "\033[0;34mUsing the Oh My Zsh template file and adding it to ~/.zshrc\033[0m"
-cp $ZSH/templates/zshrc.zsh-template ~/.zshrc
-sed -i -e "/^ZSH=/ c\\
-ZSH=$ZSH
-" ~/.zshrc
-
-echo "\033[0;34mCopying your current PATH and adding it to the end of ~/.zshrc for you.\033[0m"
-sed -i -e "/export PATH=/ c\\
-export PATH=\"$PATH\"
-" ~/.zshrc
-
-echo "\033[0;34mTime to change your default shell to zsh!\033[0m"
-chsh -s `which zsh`
-
-echo "\033[0;32m"'         __                                     __   '"\033[0m"
-echo "\033[0;32m"'  ____  / /_     ____ ___  __  __   ____  _____/ /_  '"\033[0m"
-echo "\033[0;32m"' / __ \/ __ \   / __ `__ \/ / / /  /_  / / ___/ __ \ '"\033[0m"
-echo "\033[0;32m"'/ /_/ / / / /  / / / / / / /_/ /    / /_(__  ) / / / '"\033[0m"
-echo "\033[0;32m"'\____/_/ /_/  /_/ /_/ /_/\__, /    /___/____/_/ /_/  '"\033[0m"
-echo "\033[0;32m"'                        /____/                       ....is now installed!'"\033[0m"
-echo "\n\n \033[0;32mPlease look over the ~/.zshrc file to select plugins, themes, and options.\033[0m"
-echo "\n\n \033[0;32mp.s. Follow us at http://twitter.com/ohmyzsh.\033[0m"
-/usr/bin/env zsh
-. ~/.zshrc
+echo "\033[032m"'Oh My Dotfiles have now been downloaded'"\033[0m"
