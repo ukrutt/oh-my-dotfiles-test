0991448076114c006f75c013a290d4fef1243ba2
Henrik Holm <henrik@henrikholm.com>
2014-04-14 13:55:41 -0400

    Script for doing arg testing.  Behaving unpredictably.

---
+++ scripts/test.sh
@@ -0,0 +1,26 @@
+
+echo $0
+
+echo "Number of arguments is $#"
+
+if [ -n $1 ]; then
+    echo "1 Length of '$1' is nonzero (${#1})"
+else
+    echo "1 Length of '$1' is zero (${#1})"
+fi
+
+if [ -z $1 ]; then
+    echo "2 Length of '$1' is zero (${#1})"
+else
+    echo "2 Length of '$1' is nonzero (${#1})"
+fi
+
+empty='e'
+
+if [ -n ${empty} ]; then
+    echo "3 Length of '$empty' is nonzero (${#empty})"
+fi
+
+if [ -z ${empty} ]; then
+    echo "4 Length of '$empty' is zero (${#empty})"
+fi
